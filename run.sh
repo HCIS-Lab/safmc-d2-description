7b561161914634bf77dda436c6a0b980804b59cf
Tsaimingchun14 <jerry110030014@gapp.nthu.edu.tw>
2024-12-25 16:52:58 +0800

    fix run.sh

--- run.sh
+++ run.sh
@@ -14,17 +14,16 @@ if [[ "$user_input" == "Y" || "$user_input" == "y" ]]; then
         echo "python not installed"
         exit 1
     fi
-
-    chmod +x px4_setup.sh
-    ./px4_setup.sh || { 
-        echo "Error: Failed to run ./setup_px4."; 
-        exit 1; 
-    }
-
     echo "World regeneration completed."
 else
     echo "World regeneration skipped."
 fi
+
+chmod +x px4_setup.sh
+./px4_setup.sh || { 
+  echo "Error: Failed to run ./setup_px4."; 
+  exit 1; 
+}
 
 cd PX4-Autopilot
 PX4_GZ_WORLD=safmc_d2 make px4_sitl gz_x500
