e24127e4776c03b485bd8ffb950048442a81db0a
En-Fu Liao <efliao@cs.nctu.edu.tw>
2024-12-31 14:38:24 +0000

    fix: run script & model uri

--- run.sh
+++ run.sh
@@ -7,11 +7,16 @@ if echo "$user_input" | grep -iq "^y"; then
 
     # Check for Python (either python3 or python) and run the script
     if command -v python3 >/dev/null; then
-        python3 ./generate_world.py
+        SDF_PATH="$(pwd)/models" python3 utils/generate_world.py
     elif command -v python >/dev/null; then
-        python ./generate_world.py
+        SDF_PATH="$(pwd)/models" python utils/generate_world.py
     else
         echo "Error: Python not found."
+        exit 1
+    fi
+
+    if [ $? -ne 0 ]; then
+        echo "Error: Python script execution failed."
         exit 1
     fi
 
@@ -22,7 +27,6 @@ fi
 
 # Copy models and world files to the appropriate directories
 echo "Copying models and world files..."
-cp -r ./models/* ./PX4-Autopilot/Tools/simulation/gz/models
 cp ./worlds/safmc_d2.sdf ./PX4-Autopilot/Tools/simulation/gz/worlds
 
 # Run PX4 Autopilot
