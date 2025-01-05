02444f678fe5ec4fe777e1de847f1d911f584a52
Enfu Liao <efliao@cs.nycu.edu.tw>
2025-01-05 07:31:49 +0000

    fix: add missing plugins to safmc_d2 world

--- px4_setup.sh
+++ px4_setup.sh
@@ -1,17 +1,43 @@
 #!/usr/bin/sh
 
+# set -e
+
 export GZ_SIM_RESOURCE_PATH=$(pwd)/models:$(pwd)/worlds
 export PX4_GZ_WORLD="safmc_d2"
 
 git submodule update --init --recursive
 bash ./PX4-Autopilot/Tools/setup/ubuntu.sh
 
-CMAKELIST_FILE="PX4-Autopilot/src/modules/simulation/gz_bridge/CMakeLists.txt"
+# world description
+read -p "Regenerate safmc_d2 world? [y/N]: " user_input
+if echo "$user_input" | grep -iq "^y"; then
+    echo "Regenerating the world..."
 
-if grep -q "$PX4_GZ_WORLD" "$CMAKELIST_FILE"; then
-    echo "$PX4_GZ_WORLD already exists in $CMAKELIST_FILE."
+    # Check for Python (either python3 or python) and run the script
+    if command -v python3 >/dev/null; then
+        SDF_PATH="$(pwd)/models" python3 utils/generate_world.py
+    elif command -v python >/dev/null; then
+        SDF_PATH="$(pwd)/models" python utils/generate_world.py
+    else
+        echo "Error: Python not found."
+        exit 1
+    fi
+
+    if [ $? -ne 0 ]; then
+        echo "Error: Python script execution failed."
+        exit 1
+    fi
+
+    echo "World regenerated."
 else
-    # Insert safmc_d2 in the set(gz_worlds ...) block
-    sed -i "/set(gz_worlds/a \ \ \ \ $PX4_GZ_WORLD" "$CMAKELIST_FILE"
-    echo "$PX4_GZ_WORLD has been added to $CMAKELIST_FILE."
+    echo "Skipped."
 fi
+echo "Copying and world file..."
+cp ./worlds/safmc_d2.sdf ./PX4-Autopilot/Tools/simulation/gz/worlds
+
+# config files
+find "./PX4-Autopilot.config" -type f | while read -r file; do
+    relative_path="${file#./PX4-Autopilot.config/}"
+    mkdir -p "./PX4-Autopilot/$(dirname "$relative_path")"
+    cp "$file" "./PX4-Autopilot/$relative_path"
+done
--- run.sh
+++ run.sh
@@ -1,34 +1,7 @@
 #!/usr/bin/sh
 
-read -p "Regenerate safmc_d2 world? [y/N]: " user_input
-
-if echo "$user_input" | grep -iq "^y"; then
-    echo "Regenerating the world..."
-
-    # Check for Python (either python3 or python) and run the script
-    if command -v python3 >/dev/null; then
-        SDF_PATH="$(pwd)/models" python3 utils/generate_world.py
-    elif command -v python >/dev/null; then
-        SDF_PATH="$(pwd)/models" python utils/generate_world.py
-    else
-        echo "Error: Python not found."
-        exit 1
-    fi
-
-    if [ $? -ne 0 ]; then
-        echo "Error: Python script execution failed."
-        exit 1
-    fi
-
-    echo "World regenerated."
-else
-    echo "Skipped."
-fi
-
-# Copy models and world files to the appropriate directories
-echo "Copying models and world files..."
-cp ./worlds/safmc_d2.sdf ./PX4-Autopilot/Tools/simulation/gz/worlds
+set -e
 
 # Run PX4 Autopilot
 cd PX4-Autopilot
-make px4_sitl gz_x500
+make px4_sitl gz_x500_safmc_d2
